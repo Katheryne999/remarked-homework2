f28f8ed06662fb4bd7da35b64a1a0dacd5e63eab
Katheryne999 <fenix.kate1985@gmail.com>
2022-05-01 20:00:17 +0300

    indtx.html

---
+++ script.js
@@ -0,0 +1,24 @@
+const numberValueFirst = +prompt ('first number');
+const numberValueSecond = +prompt ('second number');
+console. log(typeof numberValueFirst);
+console. log(typeof numberValueSecond);
+const sum = +numberValueFirst + numberValueSecond;
+alert ('sum' + sum);
+console. log('sum' + sum);
+const diff = numberValueFirst - numberValueSecond;
+alert ('diff' + diff);
+console. log('diff' + diff); 
+const mult = numberValueFirst * numberValueSecond;
+alert ('mult' + mult);
+console. log('mult' + mult);
+const div = numberValueFirst / numberValueSecond;
+alert ('div' + div);
+console. log('div' + div);
+
+
+
+
+
+
+
+
